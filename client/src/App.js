37cb10580f1ff2b1db5482378d7dfa1512b977ca
Noah Yarian <noah.yarian@gmail.com>
2021-12-23 13:07:54 -0600

    Add MUI

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import Button from '@mui/material/Button';
+import SwapHorizIcon from '@mui/icons-material/SwapHoriz';
 
 const App = () => (
-    <div>
-        <h1>Welcome to Book Barter!</h1>
-    </div>
+    <Button size="large" variant="contained" startIcon={<SwapHorizIcon />}>Trade Books</Button>
 );
 
 export default App;
