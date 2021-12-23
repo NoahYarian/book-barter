9c647b22f976f47bcfccd72322753b9816b215a6
Noah Yarian <noah.yarian@gmail.com>
2021-12-22 18:55:25 -0600

    use dotenv for PORT

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,4 +14,6 @@ app.get('/', (req, res) => {
     res.send("Welcome to the API!");
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
