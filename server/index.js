55efef0df42be2ce518de80b31ba9fffb877e7e3
Noah Yarian <noah.yarian@gmail.com>
2021-12-22 18:52:01 -0600

    initial express setup

--- server/index.js
+++ server/index.js
@@ -3,3 +3,14 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send("Welcome to the API!");
+});
+
+app.listen(5000, () => console.log("Server running on port 5000"));
