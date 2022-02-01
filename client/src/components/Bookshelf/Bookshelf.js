4c66b48ec5193d4e8c4884964022ab8d9110c41d
Noah Yarian <noah.yarian@gmail.com>
2022-01-31 23:52:58 -0600

    Condense imports, fix spelling

--- client/src/components/Bookshelf/Bookshelf.js
+++ client/src/components/Bookshelf/Bookshelf.js
@@ -2,10 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Navigate, useLocation } from 'react-router-dom';
 import Userfront from '@userfront/react';
-import Accordion from '@mui/material/Accordion';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import Typography from '@mui/material/Typography';
+import { Accordion, AccordionDetails, AccordionSummary, Typography } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 import { userLoggedIn, logOut } from "../../actions/user";
@@ -18,7 +15,7 @@ const Bookshelf = () => {
     const location = useLocation();
 
     const [currentBookId, setCurrentBookId] = useState(null);
-    const [accordianExpanded, setAccordianExpanded] = useState(false);
+    const [accordionExpanded, setAccordionExpanded] = useState(false);
 
     const user = useSelector(state => state.user);
 
@@ -43,8 +40,8 @@ const Bookshelf = () => {
 
     return (
         <div>
-            <Accordion expanded={accordianExpanded} onChange={() => setAccordianExpanded(prev => !prev)}>
-                <AccordionSummary expandIcon={<ExpandMoreIcon />} >
+            <Accordion expanded={accordionExpanded} onChange={() => setAccordionExpanded(prev => !prev)}>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
                     <Typography>Add a book</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
