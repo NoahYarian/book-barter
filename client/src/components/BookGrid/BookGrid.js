4025af8004773e21665883ee65dfda23e1913f4a
Noah Yarian <noah.yarian@gmail.com>
2022-02-01 00:18:35 -0600

    Show message on empty Bookshelf

--- client/src/components/BookGrid/BookGrid.js
+++ client/src/components/BookGrid/BookGrid.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Grid } from '@mui/material';
+import { Grid, Typography, Card } from '@mui/material';
 
 import Book from './Book/Book';
 
@@ -9,11 +9,18 @@ const BookGrid = ({ setCurrentBookId }) => {
 
     return (
         <Grid container sx={{ mb: 1 }}>
-            {books.map((book) => (
-                <Grid item key={book._id} sx={{ width: '100%', mt: 1 }}>
-                    <Book book={book} setCurrentBookId={setCurrentBookId} />
+            {books.length > 0 ?
+                books.map((book) => (
+                    <Grid item key={book._id} sx={{ width: '100%', mt: 1 }}>
+                        <Book book={book} setCurrentBookId={setCurrentBookId} />
+                    </Grid>
+                )) :
+                <Grid item sx={{ width: '100%', mt: 1 }}>
+                    <Card elevation={4} sx={{ p: 2 }}>
+                        <Typography variant="body1">Add some of your books to see them listed here!</Typography>
+                    </Card>
                 </Grid>
-            ))}
+            }
         </Grid>
     );
 }
