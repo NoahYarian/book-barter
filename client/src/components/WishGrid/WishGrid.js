a224bb514d123fef9906ea423f46cf080764b66e
Noah Yarian <noah.yarian@gmail.com>
2022-02-01 00:22:40 -0600

    Show message on empty Wishlist

--- client/src/components/WishGrid/WishGrid.js
+++ client/src/components/WishGrid/WishGrid.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Grid } from '@mui/material';
+import { Grid, Card, Typography } from '@mui/material';
 
 import Wish from './Wish/Wish';
 
@@ -9,11 +9,18 @@ const WishGrid = ({ setCurrentWishId }) => {
 
     return (
         <Grid container sx={{ mb: 1 }}>
-            {wishes.map((wish) => (
-                <Grid item key={wish._id} sx={{ width: '100%', mt: 1 }}>
-                    <Wish wish={wish} setCurrentWishId={setCurrentWishId} />
+            {wishes.length > 0 ?
+                wishes.map((wish) => (
+                    <Grid item key={wish._id} sx={{ width: '100%', mt: 1 }}>
+                        <Wish wish={wish} setCurrentWishId={setCurrentWishId} />
+                    </Grid>
+                )) :
+                <Grid item sx={{ width: '100%', mt: 1 }}>
+                    <Card elevation={4} sx={{ p: 2 }}>
+                        <Typography variant="body1">Add some titles and authors to see them listed here!</Typography>
+                    </Card>
                 </Grid>
-            ))}
+            }
         </Grid>
     );
 }
