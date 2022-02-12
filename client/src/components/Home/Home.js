48b7e40ec68f501269ef9f93a27e3a913407c7a4
Noah Yarian <noah.yarian@gmail.com>
2022-02-12 15:49:08 -0600

    Login page responsive styling

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Userfront from "@userfront/react";
-import { Typography } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 
 import booksImage from '../../images/Stack-of-Books-Clipart.jpg';
 
@@ -12,18 +12,18 @@ const Home = () => {
     });
 
     return (
-        <div>
-            <div style={{ float: "left" }}>
-                <Typography variant="h2" align="center" style={{ marginTop: "35px", fontWeight: "600" }}>Book Barter</Typography>
-                <Typography variant="body1" align="center">Where books make new friends</Typography>
+        <Grid container alignItems="center" sx={{ height: "100vh" }}>
+            <Grid item xs={12} sm={6} md={8}>
+                <Typography sx={{ typography: { xs: "h2", md: "h1" } }} align="center" style={{ marginTop: "35px", fontWeight: "600" }}>Book Barter</Typography>
+                <Typography sx={{ typography: { xs: "body1", md: "h5" } }} align="center">Where books make new friends</Typography>
                 <img src={booksImage} alt="snazzy stack of books" style={{ width: "100%" }} />
                 {/* <a href="https://www.freevector.com/stack-of-books-clipart-vector-30222">FreeVector.com</a> */}
-            </div>
-            <div>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
                 <SignupForm />
                 <Typography variant="body2" align="center" style={{ margin: "20px 0 20px 0" }}>Already have an account? <Link to="/login">Log in</Link></Typography>
-            </div>
-        </div>
+            </Grid>
+        </Grid>
     );
 }
 
